a97aa977ad1dcdd867a6dd68bcbd3c024b083fd5
OdisseasKorovesis <odkorovesis@gmail.com>
2020-08-27 14:20:28 +0300

    changed css for carousel controls

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ mybutton = document.getElementById("scrollToTopBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    console.log("mpike");
+    console.log(document.body.scrollTop);
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
