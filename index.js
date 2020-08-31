199f819895ca7fa948255b59f4664adf3516ddfd
OdisseasKorovesis <odkorovesis@gmail.com>
2020-08-31 15:46:55 +0300

    changed basic color, adjusted carousel with new images

--- index.js
+++ index.js
@@ -14,4 +14,17 @@ function topFunction() {
   document.documentElement.scrollTop = 0; 
 }
 
+(function(){
+  emailjs.init('user_fUgKsBd7qkHwcnN8O9YSE');
+})();
 
+window.onload = function () {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+      event.preventDefault();
+      // generate the contact number value
+      this.contact_number.value = Math.random() * 100000 | 0;
+      emailjs.sendForm('gmail', 'contact_form', this);
+      alert('Σας ευχαριστούμε για το ενδιαφέρον, με την πρώτη ευκαιρία θα έχετε την απάντηση μας!')
+      document.getElementById("contact-form").reset();
+  });
+}
