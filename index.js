5b71a3117c6323ac0a8f23a7735fd12f1b8cc357
OdisseasKorovesis <odkorovesis@gmail.com>
2021-02-03 13:26:27 +0200

    commiting final version of website

--- index.js
+++ index.js
@@ -23,8 +23,19 @@ window.onload = function () {
       event.preventDefault();
       // generate the contact number value
       this.contact_number.value = Math.random() * 100000 | 0;
-      emailjs.sendForm('gmail', 'contact_form', this);
+      emailjs.sendForm('service_wy2uqom', 'contact_form', this);
       alert('Σας ευχαριστούμε για το ενδιαφέρον, με την πρώτη ευκαιρία θα έχετε την απάντηση μας!')
       document.getElementById("contact-form").reset();
   });
+}
+
+window.onload = function () {
+  let sponsorDiv1 = document.querySelector('.sponsors-img--1');
+  let sponsorDiv2 = document.querySelector('.sponsors-img--2');
+  sponsorDiv1.addEventListener('click', function() {
+    window.location.href = "http://www.medical.gr";
+  });
+  sponsorDiv2.addEventListener('click', function() {
+    window.location.href = "http://www.arrowezio.com";
+  })
 }
